5fcf4c224d5e74bb11f36a206a3a2f5f1919da48
I2345 <57865481+I2345@users.noreply.github.com>
2022-04-15 13:52:38 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,44 @@
+
 local lib = {}
 local SynFunctions = {}
 
 function zigzag(X) wait(0.1) return math.acos(math.cos(X*math.pi))/math.pi end
 local counter = 0
 
-
+local getasset = getsynasset or getcustomasset
+local net = syn.request or httpget or http_request or requests
 
 local Camera = game.Workspace.CurrentCamera
 local ESP_Active = false
 
 local worldToViewportPoint = Camera.worldToViewportPoint
+
+local Drawing = Drawing
+
+local isfolder = isfolder
+local readfile = readfile
+local writefile = writefile
+local appendfile = appendfile
+local loadfile = loadfile
+local listfiles = listfiles
+local isfile = isfile
+local makefolder = makefolder
+
+if not isfolder("FelixDrawingLib") then
+    makefolder("FelixDrawingLib")
+end
+
+if not isfolder("FelixDrawingLib/assets") then
+    makefolder("FelixDrawingLib/assets")
+end
+
+if not isfile("FelixDrawingLib/assets/animegirl.jpg") then
+    writefile("FelixDrawingLib/assets/animegirl.jpg", net({
+
+        Url = "https://raw.githubusercontent.com/I2345/FelixDrawinglib/main/assets/anime.jpg",
+        Method = "GET",
+    }).Body)
+end
 
 local HeadOffset = Vector3.new(0,.5,0)
 local LegOffset = Vector3.new(0,3,0)
@@ -136,6 +165,37 @@ function lib:BoxESP()
         coroutine.wrap(boxesp)(v, BoxOutline, Box)
 
     end
+
+end
+
+function lib:DrawAnimeWomen()
+    local SuckOnMyBalls = Instance.new("ScreenGui")
+    local Frame = Instance.new("Frame")
+    local ImageLabel = Instance.new("ImageLabel")
+    local Frame2 = Instance.new("Frame")
+
+    --Properties:
+
+    SuckOnMyBalls.Name = "SuckOnMyBalls"
+    SuckOnMyBalls.Parent = game.CoreGui
+    SuckOnMyBalls.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+    Frame.Parent = SuckOnMyBalls
+    Frame.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+    Frame.Size = UDim2.new(1, 0, 1, 0)
+
+    ImageLabel.Parent = Frame
+    ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    ImageLabel.BackgroundTransparency = 1.000
+    ImageLabel.Size = UDim2.new(1, 0, 1, 0)
+
+    Frame2.Name = "Frame2"
+    Frame2.Parent = SuckOnMyBalls
+    Frame2.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+    Frame2.Position = UDim2.new(0, 0, -1, 0)
+    Frame2.Size = UDim2.new(1, 0, 1, 0)
+
+    ImageLabel.Image = getasset("FelixDrawingLib/assets/animegirl.jpg")
 
 end
 
